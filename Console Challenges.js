5f5fbdf1e58f096ed640afbbb48c06491a9c38a7
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-02-24 15:24:07 -0700

    Create Console Challenges.js

---
+++ Console Challenges.js
@@ -0,0 +1,10 @@
+//Console Challenges
+
+var answer = 4;
+console.log('should be true:', answer === 4);
+
+var elements = document.getElementsByClassName("title");
+var target = elements[1];
+target.innerHTML = "DISCOVER YOUR INNER HACKER";
+target.style.background = "black";
+target.style.color = "white";
