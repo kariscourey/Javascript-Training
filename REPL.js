57686274dca476ad03405b5a5adc1838a469e4c5
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-02-24 15:19:07 -0700

    Create REPL.js

---
+++ REPL.js
@@ -0,0 +1,8 @@
+//REPL
+
+//read-eval-print-loop
+//interactive computer programming environment
+
+//consult console = run in the console
+
+//can use repl.it, Chrome DevTools, JSFiddle, Codepen, JSBin, etc.
